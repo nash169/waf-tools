d889219454c0c9c8ee5248b9a21331cc616c375c
Bernardo Fichera <bernardo.fichera@gmail.com>
2020-04-11 13:53:20 +0200

    Moved to C++14 standard.

--- flags.py
+++ flags.py
@@ -30,14 +30,14 @@ def check_flags(ctx):
     ctx.env["CXXFLAGS"] = ["-Wall", "-w"]
 
     if ctx.env.CXX_NAME in ["icc", "icpc"]:
-        ctx.env["CXXFLAGS"].append("-std=c++11")
+        ctx.env["CXXFLAGS"].append("-std=c++14")
         opt_flags = ["-O3", "-xHost", "-mtune=native", "-unroll", "-g"]
     elif ctx.env.CXX_NAME in ["clang"]:
-        ctx.env["CXXFLAGS"].append("-std=c++11")
+        ctx.env["CXXFLAGS"].append("-std=c++14")
         opt_flags = ["-O3", "-march=native", "-g", "-faligned-new"]
     elif ctx.env.CXX_NAME in ["gcc", "g++"]:
         gcc_v = int(ctx.env["CC_VERSION"][0] + ctx.env["CC_VERSION"][1])
-        ctx.env["CXXFLAGS"].append("-std=c++11" if gcc_v >= 47 else "-std=c++0x")
+        ctx.env["CXXFLAGS"].append("-std=c++14" if gcc_v >= 47 else "-std=c++0x")
         opt_flags = [
             "-O3",
             "-march=native",

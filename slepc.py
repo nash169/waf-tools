4e95163b8906ca4de72f7be4a6269517eb8b9fda
Bernardo <bernardo.fichera@gmail.com>
2021-07-20 15:09:11 +0200

    Fixed pkg-config search path (petsc automatic detection removed).

--- slepc.py
+++ slepc.py
@@ -21,19 +21,26 @@ def options(opt):
 @conf
 def check_slepc(ctx):
     # Set the search path
-    if ctx.options.slepc_path is None:
-        path_check = ""
-    else:
-        path_check = os.path.join(ctx.options.slepc_path, "lib/pkgconfig")
+    if ctx.options.slepc_path is not None:
+        os.environ['PKG_CONFIG_PATH'] = os.path.join(
+            ctx.options.slepc_path, "lib/pkgconfig")
+        if ctx.options.petsc_path is not None:
+            os.environ['PKG_CONFIG_PATH'] += ":" + \
+                os.path.join(ctx.options.petsc_path, "lib/pkgconfig")
 
-    ctx.check_cfg(package='SLEPc', pkg_config_path=path_check,
-                  args='--cflags --libs', uselib_store='SLEPC')
+    ctx.check_cfg(package='SLEPc', args=[
+                  '--cflags', '--libs'], uselib_store='SLEPC')
+
+    # Remove automatic PETSc detection
+    ctx.env.LIB_SLEPC = ctx.env.LIB_SLEPC[:-1]
+    ctx.env.LIBPATH_SLEPC = ctx.env.LIBPATH_SLEPC[:-1]
+    ctx.env.INCLUDES_SLEPC = ctx.env.INCLUDES_SLEPC[:-2]
 
     if ctx.env.HAVE_SLEPC and "SLEPC" not in ctx.get_env()["libs"]:
         # Check for PETSc
         if "PETSC" not in ctx.get_env()["libs"]:
             ctx.load("petsc", tooldir="waf_tools")
-            
+
         ctx.get_env()["libs"] += ["SLEPC"]
 
 

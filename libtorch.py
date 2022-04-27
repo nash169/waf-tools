f91476b3d9e098afbe22b229eab15ac768012a8c
Bernardo <bernardo.fichera@gmail.com>
2022-04-27 14:11:36 +0200

    Added support to macOS.

--- libtorch.py
+++ libtorch.py
@@ -44,12 +44,16 @@ def check_libtorch(ctx):
         path_check = [ctx.options.libtorch_path]
 
     # kernel-lib includes
-    check_include(ctx, "LIBTORCH", ["include/torch/csrc/api/include"], ["sleef.h",
+    check_include(ctx, "LIBTORCH", ["include/torch/csrc/api/include"], ["torch/script.h",
                                                                         "torch/torch.h"], path_check)
 
     # LIB Check
-    libraries = ['libtorch_cpu', 'libnnapi_backend', 'libnnpack', 'libgloo', 'libcaffe2_nvrtc', 'libpthreadpool', 'libc10_cuda', 'libqnnpack', 'libtensorpipe', 'libprotoc', 'libfmt', 'libtorch_cuda', 'libnnpack_reference_layers', 'libgmock', 'libgtest_main', 'libXNNPACK', 'libbreakpad', 'libonnx_proto', 'libtorch_python', 'libfoxi_loader', 'libtorch', 'libtorchbind_test', 'libbenchmark_main', 'libshm', 'libjitbackend_test',
-                 'libc10d_cuda_test', 'libclog', 'libtensorpipe_cuda', 'libgmock_main', 'libasmjit', 'libgtest', 'libnvrtc-builtins', 'libdnnl', 'libcaffe2_protos', 'libprotobuf', 'libprotobuf-lite', 'libgloo_cuda', 'libonnx', 'libc10', 'libtensorpipe_uv', 'libpytorch_qnnpack', 'libcpuinfo', 'libfbgemm', 'libtorch_global_deps', 'libbenchmark', 'libcpuinfo_internals', 'libbackend_with_compiler', 'libkineto', 'libbreakpad_common']
+    if ctx.env["DEST_OS"] == "darwin":
+        libraries = ['libnnpack', 'libonnx_proto', 'libtorch_python', 'libqnnpack', 'libbackend_with_compiler', 'libtorch', 'libfbjni', 'libbreakpad', 'libcpuinfo', 'libXNNPACK', 'libbenchmark', 'libdnnl', 'libtorch_global_deps', 'libiomp5', 'libfmt', 'libpthreadpool', 'libprotobuf', 'libprotoc', 'libprotobuf-lite', 'libcaffe2_protos', 'libtorch_cpu', 'libonnx', 'libtensorpipe',
+                     'libgtest_main', 'libjitbackend_test', 'libc10', 'libtorchbind_test', 'libgtest', 'libbenchmark_main', 'libshm', 'libfoxi_loader', 'libfbgemm', 'libgmock', 'libtensorpipe_uv', 'libkineto', 'libclog', 'libcpuinfo_internals', 'libbreakpad_common', 'libpytorch_jni', 'libgloo', 'libpytorch_qnnpack', 'libnnpack_reference_layers', 'libasmjit', 'libgmock_main']
+    else:
+        libraries = ['libtorch_cpu', 'libnnapi_backend', 'libnnpack', 'libgloo', 'libcaffe2_nvrtc', 'libpthreadpool', 'libc10_cuda', 'libqnnpack', 'libtensorpipe', 'libprotoc', 'libfmt', 'libtorch_cuda', 'libnnpack_reference_layers', 'libgmock', 'libgtest_main', 'libXNNPACK', 'libbreakpad', 'libonnx_proto', 'libtorch_python', 'libfoxi_loader', 'libtorch', 'libtorchbind_test', 'libbenchmark_main', 'libshm', 'libjitbackend_test',
+                     'libc10d_cuda_test', 'libclog', 'libtensorpipe_cuda', 'libgmock_main', 'libasmjit', 'libgtest', 'libnvrtc-builtins', 'libdnnl', 'libcaffe2_protos', 'libprotobuf', 'libprotobuf-lite', 'libgloo_cuda', 'libonnx', 'libc10', 'libtensorpipe_uv', 'libpytorch_qnnpack', 'libcpuinfo', 'libfbgemm', 'libtorch_global_deps', 'libbenchmark', 'libcpuinfo_internals', 'libbackend_with_compiler', 'libkineto', 'libbreakpad_common']
 
     check_lib(ctx, "LIBTORCH", "", libraries, path_check)
 

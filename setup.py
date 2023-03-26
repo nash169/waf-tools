ab7192dce6d92ee7eb6600e9fef3a9e0ed94cec2
bernardo <bernardo.fichera@gmail.com>
2023-03-26 10:22:18 +0200

    Minor.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="waf-build",
+    name="waf-tools",
     version="1.0.0",
     author="Bernardo Fichera",
     author_email="bernardo.fichera@gmail.com",
-    description="Waf Tools.",
+    description="Collection of Waf Tools.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nash169/waf-tools.git",
@@ -18,7 +18,7 @@ setuptools.setup(
     scripts=['bin/waf'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "License :: OSI Approved :: MIT LICENSE",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
